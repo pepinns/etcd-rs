5759674047d2e9adc02836ef45d783103e91c73c
fiekers <fikersfan@gmail.com>
2024-10-28 20:58:34 -0400

    update tonic build

--- build.rs
+++ build.rs
@@ -1,14 +1,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().build_server(false).compile(
-        &[
-            "proto/auth.proto",
-            "proto/kv.proto",
-            "proto/rpc.proto",
-            "proto/v3lock.proto",
-            "proto/v3election.proto",
-        ],
-        &["proto"],
-    )?;
+    tonic_build::configure()
+        .build_server(false)
+        .compile_protos(
+            &[
+                "proto/auth.proto",
+                "proto/kv.proto",
+                "proto/rpc.proto",
+                "proto/v3lock.proto",
+                "proto/v3election.proto",
+            ],
+            &["proto"],
+        )?;
 
     Ok(())
 }
